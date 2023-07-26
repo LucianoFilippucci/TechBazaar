9e3aa0f0a114eda28ca4904bec8bdeee1e7ed087
lucky-notebook <filippucci.luciano@outlook.it>
2023-07-26 13:12:04 +0200

    changes to OrderDetailsEntity.java

--- src/main/java/it/lucianofilippucci/university/techbazaar/entities/OrderDetailsEntity.java
+++ src/main/java/it/lucianofilippucci/university/techbazaar/entities/OrderDetailsEntity.java
@@ -1,119 +1,51 @@
-package it.lucianofilippucci.university.techbazaar.entities;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "order_details", schema = "techbazaar", catalog = "")
-public class OrderDetailsEntity {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Id
-    @Column(name = "detail_id")
-    private int detailId;
-    @Basic
-    @Column(name = "order_id")
-    private int orderId;
-    @Basic
-    @Column(name = "product_id")
-    private int productId;
-    @Basic
-    @Column(name = "quantity")
-    private int quantity;
-    @Basic
-    @Column(name = "unitary_price")
-    private double unitaryPrice;
-    @Basic
-    @Column(name = "total")
-    private Double total;
-    @Basic
-    @Column(name = "iva")
-    private Integer iva;
-
-    public int getDetailId() {
-        return detailId;
-    }
-
-    public void setDetailId(int detailId) {
-        this.detailId = detailId;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getUnitaryPrice() {
-        return unitaryPrice;
-    }
-
-    public void setUnitaryPrice(double unitaryPrice) {
-        this.unitaryPrice = unitaryPrice;
-    }
-
-    public Double getTotal() {
-        return total;
-    }
-
-    public void setTotal(Double total) {
-        this.total = total;
-    }
-
-    public Integer getIva() {
-        return iva;
-    }
-
-    public void setIva(Integer iva) {
-        this.iva = iva;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        OrderDetailsEntity that = (OrderDetailsEntity) o;
-
-        if (detailId != that.detailId) return false;
-        if (orderId != that.orderId) return false;
-        if (productId != that.productId) return false;
-        if (quantity != that.quantity) return false;
-        if (Double.compare(that.unitaryPrice, unitaryPrice) != 0) return false;
-        if (total != null ? !total.equals(that.total) : that.total != null) return false;
-        if (iva != null ? !iva.equals(that.iva) : that.iva != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = detailId;
-        result = 31 * result + orderId;
-        result = 31 * result + productId;
-        result = 31 * result + quantity;
-        temp = Double.doubleToLongBits(unitaryPrice);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        result = 31 * result + (total != null ? total.hashCode() : 0);
-        result = 31 * result + (iva != null ? iva.hashCode() : 0);
-        return result;
-    }
-}
+//package it.lucianofilippucci.university.techbazaar.entities;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import jakarta.persistence.*;
+//import lombok.EqualsAndHashCode;
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Getter
+//@Setter
+//@EqualsAndHashCode
+//@ToString
+//@Entity
+//@Table(name = "order_details", schema = "techbazaar")
+//public class OrderDetailsEntity {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "detail_id", nullable = false)
+//    private int orderDetailId;
+//
+//    @Basic
+//    @Column(name = "quantity", nullable = false)
+//    private int productQuantity;
+//
+//    @Basic
+//    @Column(name = "unitary_price", nullable = false)
+//    private float productUnitaryPrice;
+//
+//    @Basic
+//    @Column(name = "total", nullable = false)
+//    private float total;
+//
+//    @Basic
+//    @Column(name = "iva", nullable = false)
+//    private int iva;
+//
+//
+////    @ManyToOne
+////    @JoinColumn(name = "product_id")
+////    @JsonIgnore
+////    private ProductEntity product;
+////
+////    @ManyToOne
+////    @JoinColumn(name = "order_id")
+////    @JsonIgnore
+////    private OrderEntity order;
+//
+//
+//}
